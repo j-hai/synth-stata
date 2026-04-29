bf14c9d459dc1d28956ad016954b648530f42d5c
Jens Hainmueller <jhain@stanford.edu>
2026-04-29 09:37:35 -0700

    Import Stata Synth source as starting baseline
    Source copied from
    /Users/jhainmueller/IPL Dropbox/Jens Hainmueller/SynthPaper/Statalibrary/
    
    Top level — what gets distributed via SSC:
      synth.ado            user-facing command (Jan 2014, ~33 KB)
      synth_ll.ado         loss-function helper
      synth.sthlp          help file
      synth.pkg            SSC package descriptor
      stata.toc            SSC TOC
      lsynth_mata_subr.mlib  compiled Mata routines
      synthopt.{mac,linux32,linux64,win32,win64,plugin}  optimizer plugin
                           binaries (one per platform)
      germany.dta, smoking.dta  sample datasets
    
    plugin-src/ — C source for synthopt:
      synthopt.c, pr_loqo.{c,h}, stplugin.{c,h}
    
    Notable up front: synthopt.mac is a Mach-O universal binary with
    i386/x86_64/ppc_7400/ppc64 slices but NO arm64. Modern native Stata
    on Apple Silicon will fail to load it. Phase 2 should rebuild.

---
+++ plugin-src/synthopt.c
@@ -0,0 +1,201 @@
+#include <stdio.h>
+#include <string.h>
+#include <malloc.h>
+#include "pr_loqo.h"
+
+#include "stplugin.h"
+
+#define ERROR_READ_STATA			201
+#define ERROR_H_NOT_SYMMETRIC		202
+#define ERROR_H_C_INCOMPATIBLE		203
+#define ERROR_H_A_INCOMPATIBLE		204
+#define ERROR_A_b_INCOMPATIBLE		205
+#define ERROR_H_l_INCOMPATIBLE		207
+#define ERROR_H_u_INCOMPATIBLE		208
+#define ERROR_BOUND_SCALAR			209
+#define ERROR_MAXITER_SCALAR		210
+#define ERROR_MARGIN_SCALAR			211
+#define ERROR_SIGF_SCALAR			212
+
+
+int ReadMatrixFromStata(char* arg, ST_double * matrixout, ST_int row, ST_int col, int verbose);
+#define ALLOC_AND_READ_FROM_STATA(arg,var,row,col, retVal)	{row = SF_row(arg);  col = SF_col(arg);if (row * col == 0) return ERROR_READ_STATA; var = calloc(col * row, sizeof(ST_double));retVal = ReadMatrixFromStata(arg,var,row,col,0);}
+
+                                                              
+
+STDLL stata_call(int argc, char *argv[])
+{
+
+	int m,n;
+	ST_int row, col;
+	//check correct number and type of args
+	ST_double *c;//[n];   //arg0
+	ST_double *H;//[n*n]; //arg1
+	ST_double *A;//[n];  //arg2
+	ST_double *b;//[1]; //arg3
+	ST_double *l;//[n]; //arg4
+	ST_double *u;//[n]; //arg5
+	ST_double *dbound;	int bound; //arg6
+	ST_double *margin; //arg7
+	ST_double *dmaxiter; int maxiter; //arg8
+	ST_double *sigf; //arg9
+	int retVal;
+
+	double *primal;//[3*38]; //result (vector of W weights)
+	double *dual;//[1+(2*38)]; //result that gets ignored (dual solution)
+
+	int loqo_return;
+	int i;
+
+	ALLOC_AND_READ_FROM_STATA(argv[1],H,row,col,retVal);	//read in H matrix
+	//sanity check on H
+	n=col;
+	if (n != row)
+	{
+		SF_error("H matrix is not symmetric\n");
+		return ERROR_H_NOT_SYMMETRIC;
+	}
+
+	ALLOC_AND_READ_FROM_STATA(argv[0],c,row,col,retVal);	//read in c vector
+	if (n != row)
+	{
+		SF_error("H and c are incompatible\n");
+		return ERROR_H_C_INCOMPATIBLE;
+	//second dimension of c not checked
+	}
+
+	ALLOC_AND_READ_FROM_STATA(argv[2],A,row,col,retVal);	//read in A vector
+	m=row;
+	if (n != col)
+	{
+		SF_error("A is incompatible with H & c\n");
+		return ERROR_H_A_INCOMPATIBLE;
+	}
+
+	ALLOC_AND_READ_FROM_STATA(argv[3],b,row,col,retVal);	//read in b value
+	if (m != (row * col))
+	{
+		SF_error("b is incompatible with A & c\n");
+		return ERROR_A_b_INCOMPATIBLE;
+	}
+
+	ALLOC_AND_READ_FROM_STATA(argv[4],l,row,col,retVal);	//read in l vector
+	if (n != (row * col))
+	{
+		SF_error("l is incompatible with H & c & A");
+		return ERROR_H_l_INCOMPATIBLE;
+	}
+
+	ALLOC_AND_READ_FROM_STATA(argv[5],u,row,col,retVal);	//read in u vector
+	if (n != (row * col))
+	{
+		SF_error("u is incompatible with H & c & A & l");
+		return ERROR_H_u_INCOMPATIBLE;
+	}
+	ALLOC_AND_READ_FROM_STATA(argv[6],dbound,row,col,retVal); bound = (int) *dbound;	//read in bound value
+	if (1 != (row * col))
+	{
+		SF_error("bound should be a 1x1 matrix and an integer value");
+		return ERROR_BOUND_SCALAR;
+	}
+	ALLOC_AND_READ_FROM_STATA(argv[7],margin,row,col,retVal);	//read in margin value
+	if (1 != (row * col))
+	{
+		SF_error("margin should be a 1x1 matrix");
+		return ERROR_MARGIN_SCALAR;
+	}
+
+	ALLOC_AND_READ_FROM_STATA(argv[8],dmaxiter,row,col,retVal); maxiter = (int)*dmaxiter;//read in maxiter value
+	if (1 != (row * col))
+	{
+		SF_error("dmaxiter should be a 1x1 matrix and an integer value");
+		return ERROR_MAXITER_SCALAR;
+	}
+	ALLOC_AND_READ_FROM_STATA(argv[9],sigf,row,col,retVal);//read in sigf value
+	if (1 != (row * col))
+	{
+		SF_error("sigf should be a 1x1 matrix");
+		return ERROR_SIGF_SCALAR;
+	}
+
+	row = SF_row(argv[10]);
+	if (row != n)
+	{
+		SF_error("solution matrix is incompatible with H");
+		return ERROR_SIGF_SCALAR;
+	}
+	
+	primal = calloc(n * 3, sizeof(ST_double));
+	dual = calloc(m + (n * 2), sizeof(ST_double));
+
+	SF_display("starting quadratic optimization\n");
+	loqo_return = pr_loqo(n,m,c,H,A,b,l,u,primal,dual,2,*sigf,maxiter,*margin,bound,0);
+
+
+	//output to wsol
+	SF_display("saving matrix\n");
+	for(i=0;i<n;i++) 
+	{
+		SF_mat_store(argv[10],i+1,1,primal[i]);
+	}
+	SF_display("done\n");
+
+	return(0) ;
+
+}
+/*
+arrays in pr_loqo are as follows
+A[i][j] on a qxr matrix is A[(r*i)+j]
+in other words, the matrix:
+
+row1
+row2
+row3
+
+is stored as row1 row2 row3
+*/
+
+int ReadMatrixFromStata(char* arg, ST_double * matrixout, ST_int row, ST_int col, int verbose)
+{
+
+	int retVal;
+	ST_ubyte toOutput[80];
+
+	//todo: what if arg is greater than 80?
+	//todo: what if not a matrix?
+	int	i,j;
+
+	if (verbose)
+	{
+		SF_display("reading in matrix "); SF_display(arg);SF_display("\n");
+		sprintf_s(toOutput,80,"Number of Rows: %d \n",row);
+		SF_display(toOutput);
+		sprintf_s(toOutput,80,"Number of Column: %d \n",col);
+		SF_display(toOutput);
+	}
+
+	for (i=0;i<col;i++) 
+	{
+		for(j=0;j<row;j++)	
+		{
+		  retVal = SF_mat_el(arg,j+1,i+1,matrixout);
+		  if (retVal != 0)
+		  {
+			sprintf_s(toOutput,80,". function SF_mat_el returned error code %d on (%d,%d)\n",retVal, row,col);
+			SF_error("var: ");SF_error(arg);SF_error(toOutput);
+			return retVal;
+		  }
+		  matrixout++;
+		}
+		if (verbose)
+		{
+			SF_display("read in a row\n");
+		}
+	}
+	if (verbose)
+	{
+		sprintf_s(toOutput,80,"Last number in matrix: %.5f \n",matrixout);
+		SF_display(toOutput);
+	}
+	return 0;
+}
